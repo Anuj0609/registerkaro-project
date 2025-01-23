d9f51a47f8feddf0ec816998df1c5adf910ac474
Anuj0609 <you@example.com>
2025-01-24 03:10:25 +0530

    ManageSection

--- src/components/Manage.jsx
+++ src/components/Manage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Manage() {
   return (
-    <div className=" relative px-5 md:pl-[72px] sm:px-6 flex flex-col space-y-8 bg-[#1C4670] overflow-hidden text-white py-[58px]">
-      <div className="w-[540px] flex items-start flex-col space-y-8">
+    <div className="relative px-5 md:pl-[72px] sm:px-6 flex flex-col space-y-8 bg-[#1C4670] overflow-hidden text-white py-[58px]">
+      <div className="w-full sm:w-[540px] flex items-start flex-col space-y-8">
         <div className="font-inter text-[24px] sm:text-[32px] font-extrabold leading-[32px] sm:leading-[40px] tracking-[0.16px] text-left justify-start">
           Manage Your Services by your Mobile Phone
         </div>
@@ -12,41 +12,42 @@ export default function Manage() {
           you to stay in touch with our experts and aids you in tracking your
           progress.
         </div>
-          <div>Get the App</div>
-          <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 items-center justify-center sm:justify-start">
-            <button className="flex flex-row items-center bg-white text-black py-1 px-3 rounded space-x-2">
-              <img src="./Apple.png" alt="Apple logo" />
-              <div>
-                <div className="font-poppins text-[10px] sm:text-[11px] font-normal leading-[19.42px] text-left">
-                  Get it on
-                </div>
-                <div className="font-poppins text-[12px] sm:text-[14px] font-semibold leading-[22px] text-left">
-                  App Store
-                </div>
+        <div>Get the App</div>
+
+        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 items-center justify-center sm:justify-start">
+          <button className="flex flex-row items-center bg-white text-black py-1 px-3 rounded space-x-2">
+            <img src="./Apple.png" alt="Apple logo" />
+            <div>
+              <div className="font-poppins text-[10px] sm:text-[11px] font-normal leading-[19.42px] text-left">
+                Get it on
               </div>
-            </button>
-            <button className="flex flex-row items-center bg-white text-black py-1 px-3 rounded space-x-2">
-              <img src="Android.png" alt="Android logo" />
-              <div>
-                <div className="font-poppins text-[10px] sm:text-[11px] font-normal leading-[19.42px] text-left">
-                  Get it on
-                </div>
-                <div className="font-poppins text-[12px] sm:text-[14px] font-semibold leading-[22px] text-left">
-                  Android Store
-                </div>
+              <div className="font-poppins text-[12px] sm:text-[14px] font-semibold leading-[22px] text-left">
+                App Store
               </div>
-            </button>
+            </div>
+          </button>
+          <button className="flex flex-row items-center bg-white text-black py-1 px-3 rounded space-x-2">
+            <img src="Android.png" alt="Android logo" />
+            <div>
+              <div className="font-poppins text-[10px] sm:text-[11px] font-normal leading-[19.42px] text-left">
+                Get it on
+              </div>
+              <div className="font-poppins text-[12px] sm:text-[14px] font-semibold leading-[22px] text-left">
+                Android Store
+              </div>
+            </div>
+          </button>
         </div>
       </div>
 
       <img
         src="./Manage3.png"
-        className="absolute top-[194px] right-[391px] w-[308px] h-[619px]"
+        className="absolute top-[194px] right-[391px] w-[308px] h-[619px] hidden md:block"
         alt="Manage App"
       />
       <img
         src="Manage3.png"
-        className="absolute top-[26px] right-[52px] w-[308px] h-[619px]"
+        className="absolute top-[26px] right-[52px] w-[308px] h-[619px] hidden md:block"
         alt="Manage App"
       />
     </div>
